17e999e30b8c0e5d8aa96978d899b8d0ae6c17cd
demos1981 <50639080+demos1981@users.noreply.github.com>
2023-08-28 22:07:08 +0300

    Update dataLink.js

--- src/data/dataLink.js
+++ src/data/dataLink.js
@@ -1,1 +1,1 @@
-export const MOCAPI_URL = 'https://64d91180e947d30a2609e1ba.mockapi.io/api/versus1/Urls/';
+export const MOCAPI_URL = 'https://64d91180e947d30a2609e1ba.mockapi.io/api/versus1/blogs/';
